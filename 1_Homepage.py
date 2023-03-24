cd72e1cb4837ddd3e1ab4a892a590104000a0988
johnsnow09 <48548048+johnsnow09@users.noreply.github.com>
2023-03-24 18:17:43 +0530

    Add files via upload
    Added image & requirement for image. Updated code

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from PIL import Image
+
+############### Get Image      ###############
+
+self_image = Image.open(r'./Icons/selfie.png')
+
+############### End Image Read     ###############
 
 
 ############### Custom Functions      ###############
@@ -26,7 +33,7 @@ st.title("Welcome to the BSE Stock Analysis App")
 
 
 # calling Vertical space function
-v_spacer(height=6)
+v_spacer(height=2)
 
 
 box_left,_1 = st.columns([3,1],gap = "large")
@@ -40,9 +47,13 @@ with box_left:
     
 
 # calling Vertical space function
-v_spacer(height=6)
+v_spacer(height=2)
 
 _2,box_right = st.columns([2,2],gap = "large")
+
+with _2:
+    st.image(self_image)
+
 
 with box_right:
     st.write('<p style="color:grey;"><i>Caption:</i></p>', 
